c465999baa95018acf908655bc3d4ee5e6094ecd
Sergiy Golubchenko <galuba1@gmail.com>
2022-12-19 23:05:06 +0200

    Hello mates3 was added

--- src/main/java/galuba/Main.java
+++ src/main/java/galuba/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello mates");
         System.out.println("Hello mates2");
+        System.out.println("Hello mates3");
     }
 }
